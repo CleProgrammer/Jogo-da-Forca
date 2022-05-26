25d0d0451424d23f1c0ed995535344d5ed6e164f
Clebson Barbosa <clebsonmengao@hotmail.com>
2022-05-26 11:23:08 -0300

    First Commit

---
+++ script.js
@@ -0,0 +1,101 @@
+const c =(el)=>document.querySelector(el)
+let perde = 0
+let vence = 0
+let letrac = false
+
+let dicas = ['FRUTA', 'CÉU', 'ROUPA', 'PLANETA', 'FRUTA', 'CARRO', 'TIME', 'TIME', 'PAÍS', 'ESTADO', 'ANIMAL', 'ANIMAL']
+let palavra = ['MANGA', 'NUVEM', 'CAMISA', 'SATURNO', 'LARANJA', 'FERRARI', 'FLAMENGO', 'LIVERPOOL', 'ANGOLA', 'AMAZONAS', 'CACHORRO', 'CASCAVEL']
+let inputNewValue = [[c('#letra1')], [c('#letra2')], [c('#letra3')], [c('#letra4')], [c('#letra5')], [c('#letra6')], [c('#letra7')],
+[c('#letra8')],[c('#letra9')], [c('#letra10')]]
+
+let divClassNew = [[c('.keyq')], [c('.keyw')], [c('.keye')], [c('.keyr')], [c('.keyt')], [c('.keyy')], [c('.keyu')], [c('.keyi')],
+[c('.keyo')], [c('.keyp')], [c('.keya')], [c('.keys')], [c('.keyqd')], [c('.keyf')], [c('.keyg')], [c('.keyh')], [c('.keygi')],
+[c('.keyj')], [c('.keyk')], [c('.keyl')], [c('.keyz')], [c('.keyx')], [c('.keyc')], [c('.keyv')], [c('.keyb')], [c('.keyn')], [c('.keym')]]
+
+let letraerrada = ''
+let wordchooseseparada = ''
+let wordchoose = ''
+
+const random = (min,max) => Math.floor(Math.random() * (max - min) + min)
+wordchoose = (palavra[random(0, palavra.length)])
+wordchooseseparada = wordchoose.split('')
+let letracerta = document.querySelectorAll('.letracerta input')
+for(let i in wordchooseseparada) {
+  inputNewValue[i].value = wordchooseseparada[i]
+  letracerta[i].style.textDecorationLine = 'underline'
+}
+
+for(let i in dicas) {
+  if(wordchoose == palavra[i]) {
+    c('.dica-principal').innerHTML = dicas[i]
+  }
+}
+
+function clicar(tecla) {
+  let letra = document.querySelector(`.key${tecla}`)
+  let clasnew = document.querySelectorAll('.teclas div')
+  let f = document.querySelectorAll('.letracerta input')
+
+  for(let i in inputNewValue) {
+    if (letra.innerHTML == inputNewValue[i].value) {
+      f[i].style.textDecorationLine = 'none'
+      f[i].value = inputNewValue[i].value
+      letrac = true
+      vence ++
+      checarcerto(letra)
+    }
+  }
+
+  if(letrac == false) {
+    perde ++
+    checarerrado(letra)
+  }
+
+  for(let k in divClassNew) {
+    if(clasnew[k].style.background == 'green') {
+      newOnclick(clasnew, k)
+    } else if(clasnew[k].style.background == 'brown') {
+      newOnclick(clasnew, k)
+    }
+  }
+
+  letrac = false
+
+  if (perde == 1) {
+    c('.cabeca').style.display = 'block'
+  } else if(perde == 2) {
+    c('.torax').style.display = 'block'
+  } else if(perde == 3) {
+    c('.braco').style.display = 'block'
+  } else if(perde == 4) {
+    c('.braco2').style.display = 'block'
+  } else if(perde == 5) {
+    c('.perna').style.display = 'block'
+  } else if(perde == 6) {
+    c('.perna2').style.display = 'block'
+    c('.resp').innerHTML = 'VOCÊ PERDEU!'
+    c('.perdeu').innerHTML = 'A palavra é: <br/>'+wordchoose
+  }
+
+  if(vence == wordchooseseparada.length) {
+    c('.resp').innerHTML = 'VOCÊ VENCEU!'
+  }
+}
+
+function checarcerto(letter) {
+  letter.style.background = 'green'
+  letter.style.cursor = 'default'
+}
+
+function checarerrado(letter) {
+  letter.style.background = 'brown'
+  letter.style.cursor = 'default'
+}
+
+function newOnclick(click, c) {
+  click[c].removeAttribute('onclick')
+}
+
+function rec() {
+  location.reload()
+}
